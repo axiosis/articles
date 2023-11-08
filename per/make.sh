4f342f019ba3d022286c6fc569b4e1191db6104c
5HT <maxim@synrc.com>
2023-11-08 23:07:52 +0200

    qn

--- per/make.sh
+++ per/make.sh
@@ -1,3 +1,3 @@
-xelatex --interaction nonstopmode anno_ua.tex
+pdflatex --interaction nonstopmode per_ua.tex
 xelatex --interaction nonstopmode anno_en.tex
 xelatex --interaction nonstopmode mltt.tex
